f10aabeb3d9640ac2ff7d86d430120a85eb08bef
cyynnthhiaaa1 <36436486+cyynnthhiaaa1@users.noreply.github.com>
2020-04-13 21:09:53 -0700

    Add files via upload
    initial commit

---
+++ Line.swift
@@ -0,0 +1,18 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Cynthia  Saldana  on 4/11/20.
+//  Copyright © 2020 Cynthia  Saldana . All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+
+struct Line{
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Cynthia  Saldana  on 4/11/20.
+//  Copyright © 2020 Cynthia  Saldana . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
